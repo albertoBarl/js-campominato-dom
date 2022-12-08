c27b9d650347bedb57c68ef8231e8b42b7e9551b
albertoBarl <alberto.barletta00@gmail.com>
2022-12-08 19:18:02 +0100

    optimized code, ready to start the ex

--- js/script.js
+++ js/script.js
@@ -8,67 +8,110 @@
 // 3."Numerare" ogni cella
 // 3.Al click di una cella, essa si colora di azzurro, resituendo il numero di cella in console
 
+
+// JS-CAMPOMINATO-GRID (v2.1)
 let play_button = document.getElementById('play-button');
+
+const grid = document.createElement('div');
+grid.classList.add('c-square');
+
 let level = document.getElementById('Livello');
-const container = document.querySelector('div.c-square');
+
+// function which create a square
+function sqCreator(){
+    let element= document.createElement('div');
+    element.classList.add('sq');
+
+    return element;
+}
+
+// function which create a grid
+function gridCreator(sqQuantity, sqRow){
+    grid.innerHTML = ''
+
+    grid.style.width= `calc(50px * ${sqRow})`
+    // ========================================   
+    for(let i=0; i<sqQuantity; i++){
+        let square= sqCreator(sqQuantity);
+        square.classList.add('d-flex', 'justify-content-center', 'align-items-center');
+        grid.appendChild(square);
+        
+        // function 'click' on the created square
+        square.addEventListener('click', function(){
+            this.classList.add('sq-aqua');
+            this.innerText= i+1;
+            console.log(this.innerText);
+        })
+    }
+    
+    // ==================================================
+    document.getElementById('main-c').appendChild(grid);
+
+    console.log(sqQuantity)
+}
 
 play_button.addEventListener('click', function(){
-
-    document.querySelector('.c-square').innerHTML = ''
-    // LIVELLO 3, GRIGLIA 10X10
-    if(level.value == 'Level_3'){
-        for(let i=0; i<100; i++){
-            let square= document.createElement('button');
-            square.classList.add('sq', 'sq-red3', 'd-flex', 'justify-content-center', 'align-items-center');
-            container.appendChild(square);
-            
-            // funzione 'click' a griglia creata
-            square.addEventListener('click', function(){
-                this.classList.add('sq-aqua');
-                this.innerText= i+1;
-                console.log(this.innerText);
-            })
-        }
-    }
-
-    // LIVELLO 2, GRIGLIA 9X9
-    else if(level.value == 'Level_2'){
-        for(let i=0; i<81; i++){
-            let square= document.createElement('button');
-            square.classList.add('sq', 'sq-red2', 'd-flex', 'justify-content-center', 'align-items-center');
-            container.appendChild(square);
-            
-            // funzione 'click' a griglia creata
-            square.addEventListener('click', function(){
-                this.classList.add('sq-aqua', 'text-dark');
-                this.innerText= i+1;
-                console.log(this.innerText);
-            })
-        }
-    }
-
-    // LIVELLO 1, GRIGLIA 7X7
-    else if(level.value == 'Level_1'){
-        for(let i=0; i<49; i++){
-            let square= document.createElement('button');
-            square.classList.add('sq', 'sq-red1', 'd-flex', 'justify-content-center', 'align-items-center');
-            container.appendChild(square);
-            
-            // funzione 'click' a griglia creata
-            square.addEventListener('click', function(){
-                this.classList.add('sq-aqua', 'text-dark');
-                this.innerText= i+1;
-                console.log(this.innerText);
-            })
-        }
-    }
-    else{
-        alert('Scegli un livello di difficoltà');
-    }
+    difficultySelector();
 })
 
+// function which select the difficulty
+
+function difficultySelector(){
+    let difficulty = level.value
+
+    let sqQuantity;
+    let sqRow;
+
+    switch(difficulty){
+        case 'Level_1':
+            sqQuantity = 100;
+            sqRow = 10;
+            break;
+        case 'Level_2':
+            sqQuantity = 81;
+            sqRow = 9;
+            break;
+        case 'Level_3':
+            sqQuantity = 49;
+            sqRow = 7;
+            break;
+        default:
+            sqQuantity = 100;
+            sqRow = 10;
+            break;
+    }
+    gridCreator(sqQuantity, sqRow);
+}
 
 
+
+
+
+
+
+
+
+
+
+// JS-CAMPOMINATO-DOM
+// 1. Generare 16 numeri casuali
+// function createBombsArray(min, max){
+//     let i=0;
+//     let bombs={}
+
+//     while(i < 16){
+//         let number = Math.floor(Math.random() * (max - min +1)) + min;
+//         if(!bombs.includes(number)) //se number non è incluso (!arrayBombs) nell'arrayBombs
+//         {
+//             bombs.push(number);
+//             i++;
+//         }
+
+//     }
+//     return bombs
+// }
+
+// 2.
 
 
 
